497e0a80a1a44c59a55e92efb925fb12704c931f
rene hernandez <rhernandez@onenorth.com>
2018-01-06 16:17:45 -0600

    adding fancy cursor

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,17 +3,20 @@
     <navigation />
     <nuxt/>
     <site-footer />
+    <fancy-cursor />
   </div>
 </template>
 
 <script>
 import Navigation from '@/components/Navigation.vue'
 import SiteFooter from '@/components/SiteFooter.vue'
+import FancyCursor from '@/components/FancyCursor.vue'
 
 export default {
   components: {
     Navigation,
-    SiteFooter
+    SiteFooter,
+    FancyCursor
   }
 }
 </script>
--- layouts/white.vue
+++ layouts/white.vue
@@ -2,15 +2,18 @@
   <div class="layout-detail--white">
     <navigation />
     <nuxt/>
+    <fancy-cursor />
   </div>
 </template>
 
 <script>
 import Navigation from '@/components/Navigation.vue'
+import FancyCursor from '@/components/FancyCursor.vue'
 
 export default {
   components: {
-    Navigation
+    Navigation,
+    FancyCursor
   }
 }
 </script>
