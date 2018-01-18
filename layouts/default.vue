bd78500e0d3c3eeab218432bec903f5965dd7bd0
rene hernandez <rhernandez@onenorth.com>
2018-01-17 18:02:51 -0600

    readding page transitions

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,29 +13,29 @@ import SiteFooter from '@/components/SiteFooter.vue'
 import FancyCursor from '@/components/FancyCursor.vue'
 
 export default {
-  transition: {
-    css: false,
-    beforeEnter: function (el) {
-      console.log(el)
-      // ...
-    },
-    // the done callback is optional when
-    // used in combination with CSS
-    enter: function (el, done) {
-      console.log(el)
-      // ...
-      done()
-    },
+  // transition: {
+  //   css: false,
+  //   beforeEnter: function (el) {
+  //     console.log(el)
+  //     // ...
+  //   },
+  //   // the done callback is optional when
+  //   // used in combination with CSS
+  //   enter: function (el, done) {
+  //     console.log(el)
+  //     // ...
+  //     done()
+  //   },
 
-    // --------
-    // LEAVING
-    // --------
+  //   // --------
+  //   // LEAVING
+  //   // --------
 
-    beforeLeave: function (el) {
-      console.log(el)
-      // ...
-    }
-  },
+  //   beforeLeave: function (el) {
+  //     console.log(el)
+  //     // ...
+  //   }
+  // },
   components: {
     Navigation,
     SiteFooter,
