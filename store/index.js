8366cf738e1869e104fa03412f2ce80284dfd7b3
Rene Hernandez <anayrene@renes-air.attlocal.net>
2019-10-10 23:55:01 -0500

    adding homepage

--- store/index.js
+++ store/index.js
@@ -1,16 +1,24 @@
-import Vuex from 'vuex'
-import content from '@/static/content.json'
-import workLanding from '@/static/_pages/work.json'
-import workPages from '@/static/_pages/workData.json'
+import Vuex from "vuex";
+import content from "@/static/content.json";
+import workLanding from "@/static/_pages/work.json";
+import workPages from "@/static/_pages/workData.json";
 
 const createStore = () => {
   return new Vuex.Store({
     state: {
       ...content,
+      navigation: {
+        logo: "Michael",
+        labels: {
+          work: "Work",
+          contact: "Contact",
+          about: "About"
+        }
+      },
       workLanding,
       workPages
     }
-  })
-}
+  });
+};
 
-export default createStore
+export default createStore;
